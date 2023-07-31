1bf867a0fd0222dfe1bdfc85fb00c232bed373d3
saem <saemilee01@gmail.com>
2023-08-01 02:10:42 +0900

    chore: eslint, prettier 설정

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,8 +1,10 @@
-const SignIn = () =>{
-  return <>
-  <input data-testid="email-input" type="id"></input>
-  <input data-testid="password-input" type="password"></input>
-  <button data-testid="signup-button">회원가입</button>
-  </>
-}
+const SignIn = () => {
+  return (
+    <>
+      <input data-testid="email-input" type="id"></input>
+      <input data-testid="password-input" type="password"></input>
+      <button data-testid="signup-button">회원가입</button>
+    </>
+  );
+};
 export default SignIn;
