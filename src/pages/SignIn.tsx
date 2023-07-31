86fe3e6712b0a1f9369710862f4a060d535c7068
saem <saemilee01@gmail.com>
2023-08-01 02:01:32 +0900

    feat: sign in 돔 요소 추가

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,8 @@
+const SignIn = () =>{
+  return <>
+  <input data-testid="email-input" type="id"></input>
+  <input data-testid="password-input" type="password"></input>
+  <button data-testid="signup-button">회원가입</button>
+  </>
+}
+export default SignIn;
