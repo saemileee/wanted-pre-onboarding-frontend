18384a4f8691117328eee286fed750eed54a18ba
saem <saemilee01@gmail.com>
2023-08-02 00:09:38 +0900

    Feat: 회원가입 컴포넌트 추가

---
+++ src/components/signUp/index.tsx
@@ -0,0 +1,20 @@
+import { EmailInputContainer, PasswordInputContainer, SubmitInput } from "../common/auth";
+import useAuthForm from "../../hooks/useAuthForm";
+
+const SignUpForm = () => {
+  const { handleSubmit } = useAuthForm();
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit();
+      }}
+    >
+      <EmailInputContainer dataTestId="email-input" />
+      <PasswordInputContainer dataTestId="password-input" />
+      <SubmitInput dataTestId="signup-button" text="회원가입" />
+    </form>
+  );
+};
+
+export default SignUpForm;
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,4 +1,11 @@
-const SignUp = () =>{
-  return <>Signup</>
-}
+import SignUpForm from "../components/signUp";
+import AuthContextProvider from "../contexts/AuthProvider/provider";
+
+const SignUp = () => {
+  return (
+    <AuthContextProvider>
+      <SignUpForm />
+    </AuthContextProvider>
+  );
+};
 export default SignUp;
