ea8dbb793b9879beac2b4271f925f748a1830c0e
saem <saemilee01@gmail.com>
2023-08-03 03:29:50 +0900

    Refactor: Todo Context 타입 지정

--- src/contexts/TodoContext/context.ts
+++ src/contexts/TodoContext/context.ts
@@ -1,5 +1,13 @@
-import { createContext } from 'react';
+import { Context, Dispatch, SetStateAction, createContext } from 'react';
+import * as TodoType from '../../interface/Todo';
 
-export const TodoContext = createContext<any | null>(null);
+interface TodoContextType {
+  todos: TodoType.Item[];
+  setTodos: Dispatch<SetStateAction<TodoType.Item[]>>;
+}
+
+export const TodoContext: Context<TodoContextType> = createContext<TodoContextType>(
+  {} as TodoContextType
+);
 
 export default {};
