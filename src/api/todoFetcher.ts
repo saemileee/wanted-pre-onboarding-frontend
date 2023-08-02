482750cf09ce01254042206ec0626473d5d05057
saem <saemilee01@gmail.com>
2023-08-02 19:41:26 +0900

    Fix: todoFetcher 함수에 requiredToken 매개변수 추가

--- src/api/todoFetcher.ts
+++ src/api/todoFetcher.ts
@@ -4,21 +4,21 @@ import * as TodoType from '../interface/Todo';
 const API_KEY = 'http://localhost:8000';
 
 export async function createTodo(todo: TodoType.Item) {
-  const response = await Api.post(API_KEY, '/todos', todo);
+  const response = await Api.post(API_KEY, '/todos', todo, true);
   return response;
 }
 
 export async function getTodos() {
-  const response = await Api.get(API_KEY, '/todos');
+  const response = await Api.get(API_KEY, '/todos', true);
   return response;
 }
 
 export async function updateTodo(todo: TodoType.Item) {
-  const response = await Api.put(API_KEY, '/todos', todo);
+  const response = await Api.put(API_KEY, '/todos', todo, true);
   return response;
 }
 
 export async function deleteTodo(id: number) {
-  const response = await Api.delete(API_KEY, `/todos/${id}`);
+  const response = await Api.delete(API_KEY, `/todos/${id}`, true);
   return response;
 }
