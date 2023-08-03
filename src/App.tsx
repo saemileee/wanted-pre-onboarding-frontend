63bd015fc5f14ed74ba81cdc1fb0dc5ca7be9a3f
saem <saemilee01@gmail.com>
2023-08-04 02:57:27 +0900

    Feat: lazy loading 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import './styles/common/_reset.css';
 import './styles/common/_global.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import Todo from './pages/Todo';
+
+const Todo = lazy(() => import('./pages/Todo'));
 
 function App() {
   return (
@@ -13,7 +14,14 @@ function App() {
         <Route path="/" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/todo" element={<Todo />} />
+        <Route
+          path="/todo"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Todo />
+            </Suspense>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
