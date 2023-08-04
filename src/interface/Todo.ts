c27d698615374c17e6489d3bf87d2fbdba69ef3f
saem <saemilee01@gmail.com>
2023-08-05 01:05:00 +0900

    Feat: updateTodo 타입 추가

--- src/interface/Todo.ts
+++ src/interface/Todo.ts
@@ -2,16 +2,14 @@ export interface Item {
   id: number;
   todo: string;
   isCompleted: boolean;
-  userId: number;
-  isEditMode?: boolean;
+  userId?: number;
 }
 
-export interface UpdateTodo {
+export interface UpdatedTodo {
   id?: number;
   todo?: string;
   isCompleted?: boolean;
   userId?: number;
-  isEditMode?: boolean;
 }
 
 export default {};
