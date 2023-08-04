5c75decd79ff6f23974fe17b0870a927c02916b5
saem <saemilee01@gmail.com>
2023-08-05 02:37:12 +0900

    Refactor: req 타입 재설정

--- src/api/authFetcher.ts
+++ src/api/authFetcher.ts
@@ -2,12 +2,12 @@ import * as Api from './api';
 import * as AuthType from '../interface/Auth';
 import API_KEY from '../constants/apiKey';
 
-export async function postSignUp(formData: AuthType.Form) {
+export async function postSignUp(formData: AuthType.FormRequest) {
   const response = await Api.post(API_KEY, '/auth/signup', formData);
   return response;
 }
 
-export async function postSignIn(formData: AuthType.Form) {
+export async function postSignIn(formData: AuthType.FormRequest) {
   const response = await Api.post(API_KEY, '/auth/signin', formData);
   return response;
 }
