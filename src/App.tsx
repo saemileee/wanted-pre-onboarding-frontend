3ca02c1c23bae2e80dd2fa28b1deec4f6277e70e
saem <saemilee01@gmail.com>
2023-08-04 15:34:16 +0900

    Refactor: 투두 레이지로딩 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import './styles/common/_reset.css';
 import './styles/common/_global.scss';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Todo from './pages/Todo';
 import useAuth from './hooks/useAuth';
 import ROUTES from './constants/routes';
-
-const Todo = lazy(() => import('./pages/Todo'));
 
 function App() {
   const { isLoggedIn } = useAuth();
   return (
     <BrowserRouter>
-      <Suspense fallback={<div style={{ color: 'white' }}>Loading...</div>}>
-        <Routes>
-          <Route path="/todo" element={<Todo />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
       <Routes>
         <Route path="/" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
