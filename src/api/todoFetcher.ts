a5afdce360aff4de2128e234e96ad14040d4939c
saem <saemilee01@gmail.com>
2023-08-05 01:01:22 +0900

    Refactor: 불필요한 req 제거

--- src/api/todoFetcher.ts
+++ src/api/todoFetcher.ts
@@ -2,17 +2,17 @@ import * as Api from './api';
 import * as TodoType from '../interface/Todo';
 import API_KEY from '../constants/apiKey';
 
-export async function createTodo(todo: TodoType.Item) {
-  const response = await Api.post(API_KEY, '/todos', todo, true);
-  return response;
-}
-
 export async function getTodos() {
   const response = await Api.get(API_KEY, '/todos', true);
   return response;
 }
 
-export async function updateTodo(id: number, todo: TodoType.UpdateTodo) {
+export async function createTodo(todo: { todo: string }) {
+  const response = await Api.post(API_KEY, '/todos', todo, true);
+  return response;
+}
+
+export async function updateTodo(id: number, todo: TodoType.UpdatedTodo) {
   const response = await Api.put(API_KEY, `/todos/${id}`, todo, true);
   return response;
 }
