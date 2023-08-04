f7b38797f6077c7b4dbebeed0bdc06920b9e8714
saem <saemilee01@gmail.com>
2023-08-05 01:04:25 +0900

    Refactor: 수정한 dispatch 커스텀훅 사용

--- src/hooks/useTodoList.ts
+++ src/hooks/useTodoList.ts
@@ -1,99 +1,51 @@
-import { useContext } from 'react';
-import * as TodoType from '../interface/Todo';
-import { TodoContext } from '../contexts/TodoContext/context';
+import { useTodoDispatch } from '../contexts/TodoContext';
 import * as todoFetcher from '../api/todoFetcher';
 
 const useTodoList = () => {
-  const context = useContext(TodoContext);
-  if (!context) {
-    throw new Error('useTodoList must be used within a TodoContextProvider');
-  }
+  const todoDispatch = useTodoDispatch();
 
-  const { todos, setTodos } = context;
-
-  const getTodos = () => {
-    const fetchData = async () => {
-      try {
-        const res = await todoFetcher.getTodos();
-        if (res.status === 200) {
-          setTodos(res.data);
-        }
-      } catch (err: any) {
-        throw new Error(err);
-      }
-    };
-    fetchData();
+  const getTodos = async () => {
+    try {
+      const res = await todoFetcher.getTodos();
+      todoDispatch({ type: 'GET', payload: res.data });
+    } catch (err: any) {
+      throw new Error(err);
+    }
   };
 
-  const addTodo = (value: string) => {
-    const newTodo: TodoType.Item = {
-      id: 0,
-      todo: value,
-      isCompleted: false,
-      userId: 0,
-      isEditMode: false,
-    };
-    const fetchData = async () => {
-      try {
-        const res = await todoFetcher.createTodo(newTodo);
-        setTodos([...todos, res.data]);
-      } catch (err: any) {
-        throw new Error(err);
-      }
-    };
-    fetchData();
+  const createTodo = async (value: string) => {
+    try {
+      const res = await todoFetcher.createTodo({ todo: value });
+      todoDispatch({ type: 'CREATE', payload: res.data });
+    } catch (err: any) {
+      throw new Error(err);
+    }
   };
 
-  const removeTodo = (id: number) => {
-    const fetchData = async () => {
-      try {
-        await todoFetcher.deleteTodo(id);
-        setTodos(todos.filter((item: TodoType.Item) => item.id !== id));
-      } catch (err: any) {
-        throw new Error(err);
-      }
-    };
-    fetchData();
+  const updateTodo = async (id: number, value: string, isCompleted: boolean) => {
+    try {
+      const req = { todo: value, isCompleted };
+      const res = await todoFetcher.updateTodo(id, req);
+      todoDispatch({ type: 'UPDATE', payload: res.data });
+    } catch (err: any) {
+      throw new Error(err);
+    }
   };
 
-  const updateTodo = (id: number, value: string, isCompleted: boolean) => {
-    const newTodo = { todo: value, isCompleted, isEditMode: false };
-    const fetchData = async () => {
-      try {
-        await todoFetcher.updateTodo(id, newTodo);
-        setTodos(
-          todos.map((item: TodoType.Item): TodoType.Item => {
-            if (item.id === id) {
-              return { ...item, todo: value, isEditMode: false, isCompleted };
-            }
-            return item;
-          })
-        );
-      } catch (err: any) {
-        throw new Error(err);
-      }
-    };
-    fetchData();
-  };
-
-  const toggleEditMode = (id: number) => {
-    setTodos(
-      todos.map((item: TodoType.Item): TodoType.Item => {
-        if (item.id === id) {
-          return { ...item, isEditMode: !item.isEditMode };
-        }
-        return item;
-      })
-    );
+  const deleteTodo = async (id: number) => {
+    try {
+      await todoFetcher.deleteTodo(id);
+      todoDispatch({ type: 'DELETE', payload: id });
+    } catch (err: any) {
+      throw new Error(err);
+    }
   };
 
   return {
     getTodos,
-    todos,
-    addTodo,
-    removeTodo,
+    createTodo,
     updateTodo,
-    toggleEditMode,
+    deleteTodo,
   };
 };
 
